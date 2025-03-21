eee220e4755519f0fa475446f62b32aea56f8050
gaeulzzang <gaeulzzang@gmail.com>
2025-03-21 15:41:33 +0900

    [Feat(세라/이가을)] useState 실습

--- week2/src/App.tsx
+++ week2/src/App.tsx
@@ -1,22 +1,36 @@
 import "./App.css";
-import List from "./components/List";
+import { useState } from "react";
 
 function App() {
-  const nickname = "세라";
-  const sweetPotato = "고구마";
-  const array = ["REACT", "NEXT", "VUE", "SVELTE", "ANGULAR", "REACT-NATIVE"];
+  const [person, setPerson] = useState({
+    name: "이가을",
+    age: 24,
+    nickname: "세라",
+    city: "",
+  });
+
+  const updateCity = () => {
+    setPerson((prev) => ({ ...prev, city: "서울" }));
+  };
+
+  const increaseAge = () => {
+    setPerson((prev) => ({ ...prev, age: prev.age + 1 }));
+  };
+
+  const decreaseAge = () => {
+    setPerson((prev) => ({ ...prev, age: prev.age - 1 }));
+  };
+
   return (
     <>
-      <strong className="school">숙명여자대학교</strong>
-      <p style={{ color: "purple", fontWeight: "bold", fontSize: "3rem" }}>
-        {nickname}/이가을
-      </p>
-      <h1>{`${nickname}는 ${sweetPotato} 아이스크림을 좋아합니다.`}</h1>
-      <ul>
-        {array.map((item, index) => (
-          <List key={index} tech={item} />
-        ))}
-      </ul>
+      <h1>이름: {person.name}</h1>
+      <h2>나이: {person.age}</h2>
+      <h3>닉네임: {person.nickname}</h3>
+      {/* 도시가 있을 경우에만 렌더링 */}
+      {person.city && <h4>도시: {person.city}</h4>}
+      <button onClick={updateCity}>도시 추가/변경</button>
+      <button onClick={increaseAge}>나이 증가</button>
+      <button onClick={decreaseAge}>나이 감소</button>
     </>
   );
 }
