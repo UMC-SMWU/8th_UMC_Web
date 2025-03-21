221bab132cb71fac2d0229019726f12a7bb89d06
gaeulzzang <gaeulzzang@gmail.com>
2025-03-21 14:50:37 +0900

    [Feat(세라/이가을)] List 컴포넌트 만들기

--- week2/src/App.tsx
+++ week2/src/App.tsx
@@ -1,9 +1,22 @@
 import "./App.css";
+import List from "./components/List";
 
 function App() {
+  const nickname = "세라";
+  const sweetPotato = "고구마";
+  const array = ["REACT", "NEXT", "VUE", "SVELTE", "ANGULAR", "REACT-NATIVE"];
   return (
     <>
-      <h1>Hello React</h1>
+      <strong className="school">숙명여자대학교</strong>
+      <p style={{ color: "purple", fontWeight: "bold", fontSize: "3rem" }}>
+        {nickname}/이가을
+      </p>
+      <h1>{`${nickname}는 ${sweetPotato} 아이스크림을 좋아합니다.`}</h1>
+      <ul>
+        {array.map((item, index) => (
+          <List key={index} tech={item} />
+        ))}
+      </ul>
     </>
   );
 }
