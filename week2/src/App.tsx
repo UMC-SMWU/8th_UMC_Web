d3e14ecac4f1eecdca73cf288950df2031a2031b
gaeulzzang <gaeulzzang@gmail.com>
2025-03-21 19:50:44 +0900

    [Feat(세라/이가을)] 미션 1 완료

--- week2/src/App.tsx
+++ week2/src/App.tsx
@@ -1,37 +1,12 @@
 import "./App.css";
-import { useState } from "react";
+import { TodoProvider } from "./context/TodoContext";
+import Todo from "./components/Todo";
 
 function App() {
-  const [person, setPerson] = useState({
-    name: "이가을",
-    age: 24,
-    nickname: "세라",
-    city: "",
-  });
-
-  const updateCity = () => {
-    setPerson((prev) => ({ ...prev, city: "서울" }));
-  };
-
-  const increaseAge = () => {
-    setPerson((prev) => ({ ...prev, age: prev.age + 1 }));
-  };
-
-  const decreaseAge = () => {
-    setPerson((prev) => ({ ...prev, age: prev.age - 1 }));
-  };
-
   return (
-    <>
-      <h1>이름: {person.name}</h1>
-      <h2>나이: {person.age}</h2>
-      <h3>닉네임: {person.nickname}</h3>
-      {/* 도시가 있을 경우에만 렌더링 */}
-      {person.city && <h4>도시: {person.city}</h4>}
-      <button onClick={updateCity}>도시 추가/변경</button>
-      <button onClick={increaseAge}>나이 증가</button>
-      <button onClick={decreaseAge}>나이 감소</button>
-    </>
+    <TodoProvider>
+      <Todo />
+    </TodoProvider>
   );
 }
 
