8105ad0086520d45aed453937e8a03a67dfe9e82
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-03-24 22:59:27 +0900

    [Feat(우리/김선화)] 첫 컴포넌트 만들기

--- week02/src/App.tsx
+++ week02/src/App.tsx
@@ -1,8 +1,23 @@
 import './App.css'
+// 1. List 컴포넌트 import
+import List from './components/List'
 
 function App() {
+  const nickname = '우리'
+  const sweetPotato = '고구마'
+  const array = ['REACT', 'NEXT', 'VUE', 'SVELTE', 'ANGULAR', 'REACT-NATIVE']
   return (
-    <></>
+     <>
+      <strong className='school'>숙명여자대학교</strong>
+      <p style={{color: 'purple', fontWeight:'bold', fontSize:'3rem'}}>{nickname}/김선화</p>
+      <h1>{`${nickname}는 ${sweetPotato} 아이스크림을 좋아합니다.`}</h1>
+      <ul>
+        {array.map((a, idx) => (
+          // 2. <li key={idx}>{a}입니다.</li> List 컴포넌트 호출
+          <List key={idx} tech={a} />
+        ))}
+      </ul>
+     </>
   )
 }
 
