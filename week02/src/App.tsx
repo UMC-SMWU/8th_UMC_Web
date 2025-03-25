ccba084603bbdcf17b4bfdfdbbc21d5ec9187b83
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-03-25 19:04:20 +0900

    [Feat(우리/김선화)] 미션2 완료

--- week02/src/App.tsx
+++ week02/src/App.tsx
@@ -1,15 +1,24 @@
 import './App.css';
 import Todo from './components/Todo';
+import Navbar from './components/Navbar';
 import { TodoProvider } from './context/TodoContext';
+import { ThemeProvider } from './context/ThemeProvider';
 
 function App() {
-  return (
-    <>
-    <TodoProvider>
-      <Todo />
-    </TodoProvider>
-    </>
-  );
+    return (
+        <>
+        <TodoProvider>
+            <ThemeProvider>
+                <div className='min-h-screen'>
+                    <Navbar />
+                    <div className='w-full'>
+                        <Todo />
+                    </div>
+                </div>
+            </ThemeProvider>
+        </TodoProvider>
+        </>
+    );
 }
 
 export default App;
--- week02/src/components/Todo.tsx
+++ week02/src/components/Todo.tsx
@@ -1,27 +1,34 @@
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
 import { useTodo } from "../context/TodoContext";
+import { THEME, useTheme } from "../context/ThemeProvider";
 
 const Todo = () => {
 	const { todos, doneTodos, completeTodo, deleteTodo } = useTodo();
+	const { theme } = useTheme();
+	
+	const isLightMode = theme === THEME.LIGHT;
+	document.body.style.backgroundColor = isLightMode ? '#eef2f3' : '#333';
+	const completeButtonColor = isLightMode ? '#28a745' : '#155724';
+  	const deleteButtonColor = isLightMode ? '#dc3545' : '#721c24';
 
 	return (
 		<div className="todo-container">
-        <h1 className="todo-container__header">TODO </h1>
+		<h1 className="todo-container__header">TODO</h1>
 			<TodoForm />
 			<div className="render-container">
 				<TodoList
 					title="할 일"
 					todos={todos}
 					buttonLabel="완료"
-					buttonColor="#28a745"
+					buttonColor={completeButtonColor}
 					onClick={completeTodo}
 				/>
 				<TodoList
 					title="완료한 일"
 					todos={doneTodos}
 					buttonLabel="삭제"
-					buttonColor="#dc3545"
+					buttonColor={deleteButtonColor}
 					onClick={deleteTodo}
 				/>
 			</div>
