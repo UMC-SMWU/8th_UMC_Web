a4e54b751bcc4472df6c58139e42381961386761
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-03-28 18:08:13 +0900

    [Feat(우리/김선화)] 영화정보 불러오기 api

--- week03/src/pages/movies.tsx
+++ week03/src/pages/movies.tsx
@@ -1,11 +1,39 @@
-import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { Movie, MovieResponse } from "../types/movie";
+import axios from "axios";
 
 const MoviePage = () => {
-    const params = useParams();
+    const [movies, setMovies] = useState<Movie[]>([]);
+
+    // 최초 실행
+    useEffect(() => {
+        const fetchMovies = async () => {
+            // 응답에 대한 타입 정의
+            const { data } = await axios.get<MovieResponse>(
+                `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${import.meta.env.VITE_MOVIE_API_KEY}`,
+                    },
+                }
+            );
+
+            setMovies(data.results);
+        };
+
+        fetchMovies();
+    }, []);
 
     return (
-        <h1>{params.movieId}번의 MoviePage</h1>
-    )
-}
+        <ul>
+            {/* optional chain 이용 */}
+            {movies?.map((movie) => (
+                <li key={movie.id}>
+                    <h1>{movie.title}</h1>
+                </li>
+            ))}
+        </ul>
+    );
+};
 
 export default MoviePage
