71f5dae915fb2d47ee9464e612b1ccabe7f23e04
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-03-31 00:34:16 +0900

    [Feat(우리/김선화)] 에러페이지 디자인

--- week03/src/pages/not-found.tsx
+++ week03/src/pages/not-found.tsx
@@ -1,6 +1,15 @@
+import { NavLink } from "react-router-dom"
+
 const NotFound = () => {
     return (
-        <h1>NotFound</h1>
+        <>
+            <div className="flex flex-col items-center justify-center h-screen gap-4">
+                <h1 className="text-3xl font-bold text-gray-500">Page Not Found</h1>
+                <NavLink to="/" className="text-blue-500">
+                    {`Go back to HomePage🏠`}
+                </NavLink>
+            </div>
+        </>
     )
 }
 
