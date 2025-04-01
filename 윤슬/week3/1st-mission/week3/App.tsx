5d388b1571d062d4651c66a7e23a0cdbe84689f8
syddojacat <165578063+syddojacat@users.noreply.github.com>
2025-04-01 17:51:10 +0900

    [Feat(윤슬/박서윤)] 3주차 폴더 추가

---
+++ 윤슬/week3/1st-mission/week3/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import MoviePage from './pages/MoviePage'
+
+function App() : React.ReactElement {
+  return (
+    <>
+    <MoviePage />
+    </>
+  )
+}
+
+export default App;
