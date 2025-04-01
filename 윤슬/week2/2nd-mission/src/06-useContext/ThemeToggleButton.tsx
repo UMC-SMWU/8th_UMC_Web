38517907c9fd4b72b32222c9d60c7689b2bb8e13
syddojacat <165578063+syddojacat@users.noreply.github.com>
2025-04-01 17:37:24 +0900

    [Feat(윤슬/박서윤)] 1,2주차 폴더 추가

---
+++ 윤슬/week2/2nd-mission/src/06-useContext/ThemeToggleButton.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { THEME, useTheme } from "./dark_context/ThemeProvider";
+import clsx from 'clsx';
+
+export default function ThemeToggleButton() : React.ReactElement {
+    const { theme, toggleTheme } = useTheme();
+
+    const isLightMode = theme === THEME.LIGHT;
+
+    return (
+        <button
+          onClick={toggleTheme}
+          className={clsx(
+            'px-4 py-2 mt-4 rounded-md transition-all',
+            {
+              'bg-black text-white': !isLightMode,
+              'bg-white text-black': isLightMode,
+            }
+          )}
+        >
+          {isLightMode ? '🌙 다크 모드' : '🌞 라이트 모드'}
+        </button>
+      );
+}
---
+++ 윤슬/week2/2nd-mission/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import ContextPage from './06-useContext/ContextPage.tsx'; // ContextPage로 변경
+
+function App(): React.ReactElement {
+  return (
+    <ContextPage />
+  );
+}
+
+export default App;
