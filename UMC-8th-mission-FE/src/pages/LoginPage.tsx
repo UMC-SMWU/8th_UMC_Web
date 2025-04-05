1f095ce8f776044c1b7c1433c09b01fc748ed070
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-04-05 21:44:11 +0900

    [Feat(우리/김선화)] week04 미션2 완료

--- UMC-8th-mission-FE/src/pages/LoginPage.tsx
+++ UMC-8th-mission-FE/src/pages/LoginPage.tsx
@@ -1,7 +1,9 @@
+import { useNavigate } from "react-router-dom";
 import useForm from "../hooks/useForm"
 import { UserSigninInformation, validateSignin } from "../utils/validate"
 
 const LoginPage = () => {
+  const navigate = useNavigate();
   const { values, errors, touched, getInputProps } = useForm<UserSigninInformation>({
     initialValue: {
       email: "",
@@ -15,13 +17,36 @@ const LoginPage = () => {
   };
 
   // 오류가 하나라도 있거나, 입려값이 비어있으면 버튼을 비활성화
-  const idDisabled = 
+  const isDisabled = 
     Object.values(errors || {}).some((error) => error.length >0) || // 오류가 있으면 true
     Object.values(values).some((value) => value === ""); // 값이 비어있으면 true
 
   return (
     <div className="flex flex-col items-center justify-center h-full gap-4 ">
       <div className="flex flex-col gap-3">
+        <div className="relative flex items-center mb-6">
+          <img 
+            className="absolute w-5 h-5"
+            src="src/assets/arrow.svg"
+            onClick={() => {navigate(-1)}}
+            style={{ cursor: "pointer" }}
+          />
+          <div className="mx-auto text-xl font-bold">
+            로그인
+          </div>
+        </div>
+        <button 
+          type="button" 
+          className="relative flex items-center w-full py-3 border border-[#2c4629] p-[10px] rounded-sm text-lg cursor-pointer hover:bg-gray-100 transition-colors duration-200"
+        >
+          <img src="src/assets/google.svg" alt="구글 아이콘" className="w-6 h-6 inline-block mr-2 absolute " />
+          <div className="mx-auto">구글 로그인</div>
+        </button>
+        <div className="flex items-center gap-10 w-full py-2">
+          <div className="flex-grow border-t border-gray-300"></div>
+          <span className="text-gray-500 text-sm">OR</span>
+          <div className="flex-grow border-t border-gray-300"></div>
+        </div>
         <input 
           {...getInputProps("email")}
           className={`border border-[#ccc] w-[300px] p-[10px] rounded-sm focus:border-[#3e623b] focus:outline-none
@@ -45,7 +70,7 @@ const LoginPage = () => {
         <button 
           type="button" 
           onClick={ handleSubmit } 
-          disabled={ idDisabled } 
+          disabled={ isDisabled } 
           className="bg-[#2c4629] text-white w-full py-3 p-[10px] rounded-sm text-lg hover:bg-[#4a6d47]/80 transition-colors duration-200 cursor-pointer disabled:bg-gray-300"
         >
           로그인
