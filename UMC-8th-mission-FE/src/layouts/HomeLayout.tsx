21a6d4e0782a4bd33bcd579e1730516bea0bb560
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-04-06 21:16:55 +0900

    [Feat(우리/김선화)] 네비게이션 바 디자인

--- UMC-8th-mission-FE/src/layouts/HomeLayout.tsx
+++ UMC-8th-mission-FE/src/layouts/HomeLayout.tsx
@@ -1,9 +1,20 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 
 const HomeLayout = () => {
+  const navigate = useNavigate();
   return (
     <div className='h-dvh flex flex-col'>
-        <nav>네비게이션바입니다</nav>
+        <nav className="flex justify-between items-center text-white font-bold outline-4 outline-lime-900 py-2">
+          <div className="text-[#2c4629] text-lg px-3">돌려돌려 LP판</div>
+          <div>
+            <button 
+              className="bg-[#2c4629] text-white py-2 mx-1 p-[10px] rounded-sm text-sm hover:bg-[#4a6d47]/80 transition-colors duration-200 cursor-pointer"
+              onClick={() => navigate("/login")}>로그인</button>
+            <button 
+              className="bg-[#2c4629] text-white py-2 mx-1 p-[10px] rounded-sm text-sm hover:bg-[#4a6d47]/80 transition-colors duration-200 cursor-pointer"
+              onClick={() => navigate("/signup")}>회원가입</button>
+          </div>
+        </nav>
         <main className='flex-1'>
           <Outlet />
         </main>
