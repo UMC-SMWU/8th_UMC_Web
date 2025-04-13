8f6aab3a73bd23f74bec53eb7a8819bfadcfc983
syddojacat <165578063+syddojacat@users.noreply.github.com>
2025-04-13 23:23:12 +0900

    [박서윤/윤슬] 4주차 정리내용 제출.

--- 윤슬/week4/2nd-mission/src/apis/axios.ts
+++ 윤슬/week4/2nd-mission/src/apis/axios.ts
@@ -4,8 +4,23 @@ import { LOCAL_STORAGE_KEY } from "../constants/key";
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_SERVER_API_URL,
   headers: {
-    Authorization: `Bearer ${localStorage.getItem(LOCAL_STORAGE_KEY.accessToken)}`,
+    "Content-Type": "application/json",
   },
 });
+
+// ✨ 요청할 때마다 토큰을 최신으로 꺼내서 붙이기
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem(LOCAL_STORAGE_KEY.accessToken);
+    if (token) {
+      config.headers = config.headers || {};
+      config.headers.Authorization = `Bearer ${JSON.parse(token)}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
