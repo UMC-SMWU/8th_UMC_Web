cc7f23c3ca874cc4bf0c6eec7bdf30f62f998649
Lee Gaeul <91470334+gaeulzzang@users.noreply.github.com>
2025-04-28 17:05:18 +0900

    Feat(세라/이가을)] 홈페이지 무한 스크롤 구현

--- week6/src/pages/HomePage.tsx
+++ week6/src/pages/HomePage.tsx
@@ -1,27 +1,70 @@
 import { useEffect, useState } from "react";
 import LpItem from "../components/LpItem";
-import useGetLps from "../hooks/queries/useGetLps";
+import useGetInfiniteLps from "../hooks/queries/useGetInfiniteLps";
 import { PAGINATION_ORDER } from "../types/common";
-import SortButton from "../components/\bSortButton";
+import SortButton from "../components/SortButton";
 import { useNavigate } from "react-router-dom";
+import { useInView } from "react-intersection-observer";
+import LpItemSkeleton from "../components/LpItemSkeleton";
 
 export default function HomePage() {
   const navigate = useNavigate();
   const [order, setOrder] = useState<PAGINATION_ORDER>(PAGINATION_ORDER.ASC);
-  const { data, refetch } = useGetLps({ order: order });
-  console.log(data);
+
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    refetch,
+    isLoading,
+  } = useGetInfiniteLps("", order, 10);
+
+  const { ref, inView } = useInView({
+    threshold: 1,
+  });
 
   useEffect(() => {
     refetch();
   }, [order, refetch]);
+
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, isFetchingNextPage, fetchNextPage]);
 
   const handleOrderChange = (newOrder: PAGINATION_ORDER) => {
     setOrder(newOrder);
   };
 
   const handleItemClick = (id: number) => {
-    console.log("Item clicked with ID:", id);
     navigate(`/lp/${id}`);
+  };
+
+  const renderItems = () => {
+    if (isLoading) {
+      return Array.from({ length: 20 }).map((_, idx) => (
+        <LpItemSkeleton key={`loading-skeleton-${idx}`} />
+      ));
+    }
+
+    if (!data) {
+      return null;
+    }
+
+    return data.pages.flatMap((page, pageIndex) =>
+      page.data.data.map((lp) => (
+        <LpItem
+          key={`${lp.id}-${pageIndex}`}
+          title={lp.title}
+          thumbnail={lp.thumbnail}
+          createdAt={lp.createdAt}
+          likes={lp.likes.length}
+          onClick={() => handleItemClick(lp.id)}
+        />
+      ))
+    );
   };
 
   return (
@@ -42,17 +85,11 @@ export default function HomePage() {
       </div>
 
       <div className="grid grid-cols-2 md:grid-cols-7 sm:grid-cols-5 xs:grid-cols-3 gap-4 text-2xl text-white">
-        {data?.data.data.map((lp) => (
-          <LpItem
-            key={lp.id}
-            title={lp.title}
-            thumbnail={lp.thumbnail}
-            createdAt={lp.createdAt}
-            likes={lp.likes.length}
-            onClick={() => handleItemClick(lp.id)}
-          />
-        ))}
+        {renderItems()}
       </div>
+
+      {/* sentinel */}
+      <div ref={ref} className="h-10" />
     </div>
   );
 }
