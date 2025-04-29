5202ec08c74187bf5e85bf81f5298a276ad86e80
Lee Gaeul <91470334+gaeulzzang@users.noreply.github.com>
2025-04-29 18:49:32 +0900

    [이가을/세라] 6주차 정리내용 제출
    * [Feat(세라/이가을)] 실습 1 구현
    
    * [Feat(세라/이가을] 실습 2 구현
    
    * [Feat(세라/이가을)] .env gitignore 추가
    
    * [Feat(세라/이가을)] 실습 3 구현
    
    * [Remove(세라/이가을)] .env 파일 git tracking 제거
    
    * [Move(세라/이가을)] 미션 1 폴더로 이동
    
    * [Feat(세라/이가을)] 실습 2,3 폴더 만들기
    
    * [Mod(세라/이가을)] modify github account
    
    * [Remove(세라/이가을)] .DS_Store 제거
    
    * [Feat(세라/이가을)] 동영상 실습 구현
    
    * [Feat(세라/이가을)] 실습 2,3 기능 구현
    
    * [Feat(세라/이가을)] navbar 홈페이지 연결
    
    * [Del(세라/이가을)] .DS_Store 제거
    
    * [Feat(세라/이가을)] 4주차 실습 5주차로 복사
    
    * [Feat(세라/이가을)] 실습 1 구현
    
    * [Feat(세라/이가을)] 실습 2 구현
    
    * [Feat(세라/이가을)] 실습 3 구현
    
    * [Feat(세라/이가을)] 6주차 폴더 생성
    
    * [Feat(세라/이가을)] .DS_Store 제거
    
    * [Feat(세라/이가을)] 사이드바 구현
    
    * Feat(세라/이가을)] lp 카드 구현
    
    * Feat(세라/이가을)] 미션 1 구현
    
    * Feat(세라/이가을)] 홈페이지 무한 스크롤 구현
    
    * [Feat(세라/이가을)] 미션 2 구현
    
    * [Remove(세라/이가을)] Remove .DS_Store

---
+++ week6/src/components/NavBar.tsx
@@ -0,0 +1,90 @@
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../context/AuthContext";
+import NavButton from "./NavButton";
+import { AlignJustify, Search } from "lucide-react";
+import { LOCAL_STORAGE_KEY } from "../constants/key";
+import { useEffect, useState } from "react";
+import SideBar from "./SideBar";
+
+export default function NavBar() {
+  const navigate = useNavigate();
+  const { accessToken, logout } = useAuthContext();
+  const [nickname, setNickname] = useState("");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [wasSidebarOpen, setWasSidebarOpen] = useState(false);
+  const toggleSidebar = () => {
+    setIsSidebarOpen(true);
+    setWasSidebarOpen(true);
+  };
+
+  useEffect(() => {
+    const storedNickname = localStorage.getItem(LOCAL_STORAGE_KEY.nickname);
+    if (storedNickname) {
+      setNickname(storedNickname);
+    }
+  }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setIsSidebarOpen(false);
+      } else {
+        setIsSidebarOpen(wasSidebarOpen);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [wasSidebarOpen]);
+
+  return (
+    <>
+      <nav className="flex items-center justify-between px-6 py-4 bg-[#0d0d0d]">
+        <div className="flex flex-1 items-center gap-4">
+          <AlignJustify color="white" onClick={toggleSidebar} />
+          <h1
+            className="text-2xl font-bold text-pink-600"
+            onClick={() => navigate("/")}
+          >
+            돌려돌려LP판
+          </h1>
+        </div>
+        <Search color="white" size={18} />
+        {accessToken ? (
+          <section className="flex gap-4 items-center">
+            <NavButton text={`${nickname}님 반갑습니다.`} onClick={() => {}} />
+            <NavButton
+              text="로그아웃"
+              onClick={() => {
+                logout();
+                localStorage.removeItem(LOCAL_STORAGE_KEY.nickname);
+              }}
+              style="bg-pink-600"
+            />
+          </section>
+        ) : (
+          <section className="flex gap-4 items-center">
+            <NavButton text="로그인" onClick={() => navigate("/login")} />
+            <NavButton
+              text="회원가입"
+              onClick={() => navigate("/signup")}
+              style="bg-pink-600"
+            />
+          </section>
+        )}
+      </nav>
+
+      {isSidebarOpen && (
+        <div
+          className="fixed inset-0 "
+          onClick={() => {
+            setIsSidebarOpen(false);
+            setWasSidebarOpen(false);
+          }}
+        />
+      )}
+      <SideBar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
+    </>
+  );
+}
