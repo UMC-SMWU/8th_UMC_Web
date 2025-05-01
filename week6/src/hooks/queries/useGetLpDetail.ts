bc4b0b4bc505e3f38b18c08350abaeac8b7cb67b
Lee Gaeul <91470334+gaeulzzang@users.noreply.github.com>
2025-05-02 00:37:55 +0900

    [Mod(세라/이가을)] useGetLpDetail query key 사용

--- week6/src/hooks/queries/useGetLpDetail.ts
+++ week6/src/hooks/queries/useGetLpDetail.ts
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { getLpById } from "../../api/LpService";
+import {QUERY_KEY} from "../../constants/key.ts";
 
 function useGetLpDetail(lpId: number) {
   return useQuery({
-    queryKey: ["lps", lpId],
+    queryKey: [QUERY_KEY.lps, lpId],
     queryFn: () => getLpById(lpId),
     staleTime: 1000 * 60 * 5,
     gcTime: 1000 * 60 * 60,
