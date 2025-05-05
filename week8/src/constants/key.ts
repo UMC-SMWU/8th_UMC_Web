7c5080408a9d4e8043f803561056302f528b3641
Lee Gaeul <91470334+gaeulzzang@users.noreply.github.com>
2025-05-05 14:28:45 +0900

    [Feat(세라/이가을)] 8주차 폴더 생성

---
+++ week8/src/constants/key.ts
@@ -0,0 +1,11 @@
+export const LOCAL_STORAGE_KEY = {
+  accessToken: "accessToken",
+  refreshToken: "refreshToken",
+  nickname: "nickname",
+};
+
+export const QUERY_KEY = {
+  lps: "lps",
+  comments: "comments",
+  myInfo: "myInfo",
+};
