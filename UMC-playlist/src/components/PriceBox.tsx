6d9b5516a4f2e298224282c056266927a3211ec6
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-05-16 01:26:30 +0900

    [Feat(우리/김선화)] week09 미션2 완료

--- UMC-playlist/src/components/PriceBox.tsx
+++ UMC-playlist/src/components/PriceBox.tsx
@@ -1,25 +1,42 @@
 import { useDispatch, useSelector } from "../hooks/useCustomRedux";
 import { clearCart } from "../slices/cartSlice";
+import { closeModal, openModal } from "../slices/modalSlice";
+import ClearModal from "./ClearModal";
 
 const PriceBox = () => {
   const { total } = useSelector((state) => state.cart);
+  const { isOpen, modalContent } = useSelector((state) => state.modal);
+
   const dispatch = useDispatch();
+
+  const handleModal = () => {
+    dispatch(openModal("clear"));
+  };
 
   const handleClearCart = () => {
     dispatch(clearCart());
+    dispatch(closeModal());
   };
+
+  const handleCloseModal = () => {
+    dispatch(closeModal());
+  }
 
   return (
     <div className="flex justify-between p-8">
       <button 
         className="border p-4 rounded-md cursor-pointer"
-        onClick={handleClearCart}
+        onClick={handleModal}
       >
           장바구니 초기화
       </button>
       <div>
         총 가격 : {total} 원
       </div>
+
+      {isOpen && modalContent === "clear" && (
+        <ClearModal onConfirm={handleClearCart} onCancle={handleCloseModal}/>
+      )}
     </div>
   )
 }
