bafa754a2c357e5cb7c2057905646cb72f30f9e8
Jinx921 <hyejin020921@sookmyung.ac.kr>
2025-05-19 00:52:58 +0900

    Refactor: 9주차 정리 내용 머지 (김선화/우리)

--- UMC-8th-mission-FE/src/components/LpCard/LpCardSkeleton.tsx
+++
@@ -1,9 +0,0 @@
-const LpCardSkeleton = () => {
-  return (
-    <div className="relative w-full pb-[100%] rounded overflow-hidden shadow-lg animate-pulse" >
-        <div className="absolute top-0 left-0 w-full h-full object-cover bg-gray-300" />
-    </div>
-  )
-}
-
-export default LpCardSkeleton
