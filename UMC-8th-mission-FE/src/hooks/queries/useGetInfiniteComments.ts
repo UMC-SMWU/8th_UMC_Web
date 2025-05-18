2d91abc7ac10099b9899247f38ae0fa139b2ed0c
SEONHWA KIM <105573067+sunhwaaRj@users.noreply.github.com>
2025-05-19 00:52:14 +0900

    [김선화/우리] 9주차 정리 내용 제출
    * [Feat(우리/김선화)] tailwindCSS 설치
    
    * [Feat(우리/김선화)] useParams
    
    * [Feat(우리/김선화)] 영화정보 불러오기 api
    
    * [Feat(우리/김선화)] week03 미션1 완료
    
    * [Feat(우리/김선화)] week03 미션2 완료
    
    * [Feat(우리/김선화)] Navbar 추가
    
    * [Feat(우리/김선화)] 에러페이지 디자인
    
    * [Feat(우리/김선화)] 영화 상세 api get
    
    * [Feat(우리/김선화)] week03 미션3 완료
    
    * [Feat(우리/김선화)] week04 미션1 완료
    
    * [Feat(우리/김선화)] 파일 초기 생성
    
    * [Feat(우리/김선화)] useForm 훅 생성
    
    * [Feat(우리/김선화)] week04 미션2 완료
    
    * [Feat(우리/김선화)] week04 미션3 기능완료
    
    * [Feat(우리/김선화)] 네비게이션 바 디자인
    
    * [Feat(우리/김선화)] 비밀번호 show 버튼
    
    * [Feat(우리/김선화)] 회원가입 성공 시 로그인 페이지 이동
    
    * [Feat(우리/김선화)] 입력 UI 순서대로 하도록 변경
    
    * [Design(우리/김선화)] 전체 테마 색상 변경
    
    * [Feat(우리/김선화)] week05 미션1 완료
    
    * [Feat(우리/김선화)] week05 미션2 완료
    
    * [Feat(우리/김선화)] week05 미션3 완료
    
    * [Feat(우리/김선화)] sidebar, footer 추가
    
    * [Feat(우리/김선화)] useQuery 데이터 불러오기
    
    * [Feat(우리/김선화)] 레이아웃, order 버튼
    
    * [Feat(우리/김선화)] 레이아웃 이름 추가
    
    * [Feat(우리/김선화)] 홈 화면 lp 목록
    
    * [Feat(우리/김선화)] lp 상세 불러오기
    
    * [Feat(우리/김선화)] lp 상세 로그인 유무 확인
    
    * [Feat(우리/김선화)] week06 미션1 완료
    
    * [Feat(우리/김선화)] 무한스크롤 구현
    
    * [Feat(우리/김선화)] 스켈레톤UI 구현
    
    * [Feat(우리/김선화)] 댓글 쿼리 설정
    
    * [Feat(우리/김선화)] lp상세 디자인
    
    * [Feat(우리/김선화)] week06 미션2 완료
    
    * [Feat(우리/김선화)] useMutation 좋아요
    
    * [Rename(우리/김선화)] 폴더 위치 변경
    
    * [Feat(우리/김선화)] postComment
    
    * [Feat(우리/김선화)] 댓글 수정, 삭제
    
    * [Feat(우리/김선화)] useMutation 실시간 적용
    
    * [Feat(우리/김선화)] 내 정보 수정
    
    * [Feat(우리/김선화)] useMutation 로그인 관리
    
    * [Feat(우리/김선화)] 탈퇴 기능 구현
    
    * [Feat(우리/김선화)] 마이페이지 lp 목록
    
    * [Feat(우리/김선화)] lp 삭제
    
    * [Feat(우리/김선화)] lp 수정
    
    * [Feat(우리/김선화)] tag 적용 문제 해결
    
    * [Feat(우리/김선화)] week07 미션 1 완료
    
    * [Feat(우리/김선화)] optimistic update 좋아요 반영
    
    * [Feat(우리/김선화)] week07 미션2 완료
    
    * [Feat(우리/김선화)] 레이아웃 닉네임 불러오기 수정
    
    * [Feat(우리/김선화)] week08 미션1 완료
    
    * [Feat(우리/김선화)] week08 미션2 완료
    
    * [Fix(우리/김선화)] lp 회전 설정 및 파일명 변경
    
    * [Feat(우리/김선화)] 초기 설정
    
    * [Feat(우리/김선화)] week09 미션1 완료
    
    * [Feat(우리/김선화)] week09 미션2 완료
    
    * [Feat(우리/김선화)] week09 미션3 완료

---
+++ UMC-8th-mission-FE/src/hooks/queries/useGetInfiniteComments.ts
@@ -0,0 +1,27 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { PAGINATION_ORDER } from "../../enums/common";
+import { QUERY_KEY } from "../../constants/key";
+import { getLpCommentList } from "../../apis/comment";
+
+function useGetInfiniteComments(
+    lpId: number,
+    limit: number,
+    order: PAGINATION_ORDER,
+) {
+    return useInfiniteQuery({
+        queryKey: [QUERY_KEY.comment, lpId, order],
+        queryFn: ({ pageParam = 0 }) => getLpCommentList(
+            lpId,
+            { cursor: pageParam, limit, order }
+        ),
+        // initialPageParam: 0,
+        getNextPageParam: (lastPage) => {
+            if (lastPage.data.hasNext) {
+                return lastPage.data.nextCursor;
+            }
+            return undefined;
+        },
+    });
+}
+
+export default useGetInfiniteComments;
---
+++ UMC-playlist/src/components/CartList.tsx
@@ -0,0 +1,18 @@
+import CartItem from "./CartItem"
+import { useCartInfo } from "../hooks/useCartStore";
+
+const CartList = () => {
+	const { cartItems } = useCartInfo();
+
+	return (
+		<div className="flex flex-col items-center">
+				<ul>
+					{cartItems.map((item) => (
+						<CartItem key={item.id} lp={item}/>
+					))}
+				</ul>
+			</div>
+	)
+}
+
+export default CartList
